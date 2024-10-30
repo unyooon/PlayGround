641f05345a864ed2b35899c33b6a58727c4f5959
unyooon <mryy720@yahoo.co.jp>
2024-10-30 16:04:10 +0900

    📈 gaがうまく言ってなかったので修正

--- ChillTime/plugins/ga.client.ts
+++ ChillTime/plugins/ga.client.ts
@@ -1,52 +1,18 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  if (process.env.NODE_ENV !== "production") return;
+import VueGtag from "vue-gtag";
 
+// Nuxtプラグインの登録
+export default defineNuxtPlugin((nuxtApp) => {
+  // ルーター取得
   const router = useRouter();
 
-  /*
-   ** Google Analytics のスクリプトを挿入
-   */
-  (function (
-    i: any,
-    s: Document,
-    o: string,
-    g: string,
-    r: string,
-    a?: HTMLScriptElement,
-    m?: Element
-  ) {
-    i["GoogleAnalyticsObject"] = r;
-    (i[r] =
-      i[r] ||
-      function () {
-        (i[r].q = i[r].q || []).push(arguments);
-      }),
-      (i[r].l = Date.now());
-    (a = s.createElement(o) as HTMLScriptElement),
-      (m = s.getElementsByTagName(o)[0]);
-    if (a) {
-      a.async = true;
-      a.src = g;
-    }
-    if (m && m.parentNode) {
-      m.parentNode.insertBefore(a, m);
-    }
-  })(
-    window,
-    document,
-    "script",
-    "https://www.google-analytics.com/analytics.js",
-    "ga"
+  // Vue登録
+  nuxtApp.vueApp.use(
+    VueGtag,
+    {
+      appName: "chillndfocus", // サイトの名称
+      pageTrackerScreenviewEnabled: true, // ページトラッキングスクリーンビューを有効
+      config: { id: `G-H7K48HGGER` }, // GoogleAnalytics(GA4)の測定IDを指定する
+    },
+    router
   );
-
-  const ga = (window as any).ga;
-  ga("create", "G-H7K48HGGER", "auto"); // 'G-XXXXXXXXXX' を取得したIDに置き換える
-
-  /*
-   ** ルーティングごとに Google Analytics にページビューを送信
-   */
-  router.afterEach((to) => {
-    ga("set", "page", to.fullPath);
-    ga("send", "pageview");
-  });
 });
